dd03fae45733291608979d062e4f9fbb12ba05c3
mh1-25 <mobyb0045@gmail.com>
2025-05-08 23:30:02 +0300

    commet for Sha3bona

---
+++ Controllers/ExamController.php
@@ -0,0 +1,73 @@
+<?php
+require_once 'DBController.php';
+require_once __DIR__ . '/../Models/Exam.php';
+class ExamController {
+    protected $db;
+
+    public function AddExam($Exam) 
+{
+    $this->db = new DBController;
+
+    if ($this->db->openConnection()) {
+
+        $Title = $Exam->getTitle();
+        $Type = $Exam->getType();
+        $Date = $Exam->getDate();
+        $CrsId = $Exam->getCrsId();
+
+        $Date = addslashes($Exam->getDate());
+
+        $query = "INSERT INTO exam VALUES 
+                ('', '$Title', '$Type' ,'$Date' , '$CrsId')";
+        $result = $this->db->insert($query);
+
+        if ($result === false) {
+            echo "Error in Query";
+        } else {
+            return "";
+        }
+    } else {
+        echo "Connection Failed";
+    }
+}
+
+    public function getAllExames(){
+        $this->db = new DbController;
+            if($this->db->openConnection()) 
+            {
+                $query = "SELECT exam.ExamId, exam.Title, course.CrsName FROM exam JOIN course ON exam.CrsId = course.CrsId";;
+
+                $result = $this->db->select($query);
+                if($result === false)
+                {
+                    return false;
+                }
+                else
+                {
+                    return $result;
+                }   
+            }
+    }
+    public function DeleteExam($ExamId) 
+    {
+        $this->db = new DBController;
+        if($this->db->openConnection())
+        {
+            $queryDeletecourse = "DELETE FROM exam WHERE ExamId = '$ExamId'";
+
+            $result = $this->db->delete($queryDeletecourse);
+
+            if($result === false)
+            {
+                echo "Error in Query";
+            }
+            else
+            {
+                return "";
+            }
+        }
+    }
+}
+
+
+?>
